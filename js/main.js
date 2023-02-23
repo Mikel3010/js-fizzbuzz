66b6b3be2279f701df4fc36ef1ea2ad441c18bf1
Mikel3010 <mikelgremi30@gmail.com>
2023-02-23 21:23:30 +0100

    bonus 1

--- js/main.js
+++ js/main.js
@@ -10,3 +10,14 @@ for (let i = 1; i <= 100; i++){
     console.log('fizz')
     else console.log(i);
 }
+
+
+for (let i = 1; i <= 100; i++){
+    let element = document.createElement('li')
+    element.innerText = "";
+    if(i % 3 === 0 ) {element.innerText += "Fizz";}
+    if(i % 5 === 0 ) {element.innerText += "Buzz";}
+    if(element.innerText === ""){element.innerText = i;}
+    document.getElementById('output').appendChild(element);
+    element.classList.add(`class-${element.innerText}`)
+  }  
